68be1b96c84de50b331ca73b4cc259434f744c96
agustin <popal9193@gmail.com>
2020-02-18 20:29:26 -0300

    puesto el key y probado

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from api import PixabayAPI
 logging.basicConfig(format='%(asctime)s.%(msecs)03d [%(threadName)s] - %(message)s', datefmt='%H:%M:%S', level=logging.INFO)
 
 carpeta_imagenes = './imagenes'
-query = 'computadoras'
-api = PixabayAPI('PONER ACÁ EL API KEY', carpeta_imagenes)
+query = 'vehiculo militar'
+api = PixabayAPI('15310851-8beba4d80305d85650c9c7cc2', carpeta_imagenes)
 
 logging.info(f'Buscando imagenes de {query}')
 urls = api.buscar_imagenes(query, 5)
